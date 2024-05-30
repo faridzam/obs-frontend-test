181d46caf14e839faf573c680e1e397829cfc069
faridzam <skinel123@gmail.com>
2024-05-31 01:35:13 +0700

    fix: screenshot assets

--- vite.config.ts
+++ vite.config.ts
@@ -4,21 +4,24 @@ import { defineConfig } from 'vite';
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  preview: {
-    port: 3001,
-    strictPort: true
-  },
-  server: {
-    port: 3001,
-    strictPort: true,
-    host: true,
-    origin: "http://0.0.0.0:3001",
-  },
-  resolve: {
-    alias: [
-      {find: '@', replacement: path.resolve(__dirname, 'src')},
-    ]
+export default defineConfig(({}) => {
+
+  return {
+    plugins: [react(), tsconfigPaths()],
+    preview: {
+      port: 3001,
+      strictPort: true
+    },
+    server: {
+      port: 3001,
+      strictPort: true,
+      host: true,
+      origin: "http://localhost:3001",
+    },
+    resolve: {
+      alias: [
+        {find: '@', replacement: path.resolve(__dirname, 'src')},
+      ]
+    }
   }
 })
