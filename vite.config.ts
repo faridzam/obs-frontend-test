785bd95046a5bde5012082ec4da18b4325dfd142
faridzam <skinel123@gmail.com>
2024-05-30 21:14:00 +0700

    chore: vite config for expose port

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,16 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  preview: {
+    port: 3001,
+    strictPort: true
+  },
+  server: {
+    port: 3001,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:3001",
+  },
   resolve: {
     alias: [
       {find: '@', replacement: path.resolve(__dirname, 'src')},
